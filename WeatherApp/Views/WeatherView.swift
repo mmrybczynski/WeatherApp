e02557f36f2bccdc22de95eceae2b94c15ac70e2
Mateusz <mateusz.rybczynski@icloud.com>
2023-11-02 17:01:06 +0100

    Added packages and weather page

---
+++ WeatherApp/Views/WeatherView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Mateusz Rybczyński on 02/11/2023.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeatherView()
+}
