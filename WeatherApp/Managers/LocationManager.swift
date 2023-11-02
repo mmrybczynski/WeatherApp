8ac6840c19e1fae094d2a1d561346c171d124e00
Mateusz <mateusz.rybczynski@icloud.com>
2023-11-02 17:37:39 +0100

    Added LocationManager

--- WeatherApp/Managers/LocationManager.swift
+++ WeatherApp/Managers/LocationManager.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject {
+    private var manager = CLLocationManager()
+    @Published var userLocation: CLLocation?
+    static let shared = LocationManager()
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
+    }
+    
+    func requestLocation() {
+        manager.requestWhenInUseAuthorization()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            print("DEBUG: Not determined")
+        case .restricted:
+            print("DEBUG: Restricted")
+        case .denied:
+            print("DEBUG: Denied")
+        case .authorizedWhenInUse:
+            print("DEBUG: When in use")
+        case .authorizedAlways:
+            print("DEBUG: Always")
+        default:
+            break
+            
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else {return}
+        self.userLocation = location
+    }
+}
