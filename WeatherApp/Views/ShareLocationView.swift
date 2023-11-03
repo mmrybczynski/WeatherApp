8c64126b2443e21860a5193b32b32f9360e121e8
Mateusz <mateusz.rybczynski@icloud.com>
2023-11-03 17:43:43 +0100

    Added locationView and location request to user

---
+++ WeatherApp/Views/ShareLocationView.swift
@@ -0,0 +1,18 @@
+//
+//  ShareLocationView.swift
+//  WeatherApp
+//
+//  Created by Mateusz Rybczyński on 03/11/2023.
+//
+
+import SwiftUI
+
+struct ShareLocationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShareLocationView()
+}
